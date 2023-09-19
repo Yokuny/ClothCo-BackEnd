c4783f5ef7c489ee1ed6382a2e3ee2f56eadb613
Felipe Rangel Ribeiro <felipe.vni@hotmail.com>
2023-09-19 18:11:14 -0300

    fix: Nestjs returning to Nodejs project

--- src/products/dto/mongoId.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsMongoId } from 'class-validator';
-import { ApiProperty } from '@nestjs/swagger';
-
-export class mongoIdDto {
-  @ApiProperty({ description: 'MongoDB _id' })
-  @ApiProperty({ example: '6451786d24e368d53c9b4d66' })
-  @IsMongoId()
-  id: string;
-}
